d118e9ec7d41bb8ca23978dde7479fd861aed987
Tim La <timla111@Tims-Air-2.fios-router.home>
2022-02-14 11:33:50 -0500

    bar chart created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import './App.css';
+import BarChart from './components/BarChart';
+import {UserData} from './Data'
 
 function App() {
+
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [{
+      label: "Users Gained",
+      data: UserData.map((data) => data.userGain),
+    }],
+  })
+
   return (
     <div className="App">
-      <h1>Test</h1>
+      <div style={{width: "700px"}}>
+        <BarChart chartData={userData}/>
+      </div>
     </div>
   );
 }
