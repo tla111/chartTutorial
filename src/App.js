d72e5aeddf8ceab796ac0f0a71420b353f9667a5
Tim La <timla111@Tims-Air-2.fios-router.home>
2022-02-14 11:51:23 -0500

    line chart created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import './App.css';
 import BarChart from './components/BarChart';
 import LineChart from './components/LineChart';
+import PieChart from './components/PieChart';
 import {UserData} from './Data'
 
 function App() {
 
-  const [userData, setUserData] = useState({
+  const [userGainedData, setUserGainedData] = useState({
     labels: UserData.map((data) => data.year),
     datasets: [{
       label: "Users Gained",
@@ -17,13 +18,27 @@ function App() {
     }],
   })
 
+  const [userLostData, setUserLostData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [{
+      label: "Users Lost",
+      data: UserData.map((data) => data.userLost),
+      backgroundColor: ["gray"],
+      borderColor: "black",
+      borderWidth: 2,
+    }],
+  })
+
   return (
     <div className="App">
-      <div style={{width: "700px"}}>
-        <BarChart chartData={userData}/>
+      <div style={{width: "550px", margin: "7px"}}>
+        <BarChart chartData={userGainedData}/>
       </div>
-      <div style={{width: "700px"}}>
-        <LineChart chartData={userData}/>
+      <div style={{width: "550px", margin: "7px"}}>
+        <LineChart chartData={userLostData}/>
+      </div>
+      <div style={{width: "500px", margin: "7px"}}>
+        <PieChart chartData={userGainedData}/>
       </div>
     </div>
   );
